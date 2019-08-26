2952f1e286be803b00a9d9c1415315488f9668f6
María José Paschcuan <majopaskuvan@gmail.com>
2019-08-26 09:03:50 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+    $('#load_data').click(function(){
+     $.ajax({
+      url:"nomina.csv",
+      dataType:"text",
+      success:function(data)
+      {
+       var nomina_data = data.split(/\r?\n|\r/);
+       var table_data = '<table class="table table-hover">';
+       for(var count = 0; count<nomina_data.length; count++)
+       {
+        var cell_data = nomina_data[count].split(",");
+        table_data += '<tr>';
+        for(var cell_count=0; cell_count<cell_data.length; cell_count++)
+        {
+         if(count === 0)
+         {
+          table_data += '<th>'+cell_data[cell_count]+'</th>';
+         }
+         else
+         {
+          table_data += '<td>'+cell_data[cell_count]+'</td>';
+         }
+        }
+        table_data += '</tr>';
+       }
+       table_data += '</table>';
+       $('#nomina_table').html(table_data);
+      }
+     });
+    });
+    
+   });
+   
